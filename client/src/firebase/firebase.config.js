deaed9234d0d33606e702afa1a0ad3ac2f84a6fc
as4c <sagarkumarsameer@gmail.com>
2024-05-12 00:04:57 +0530

    env resoloved

--- client/src/firebase/firebase.config.js
+++ client/src/firebase/firebase.config.js
@@ -1,19 +1,16 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
-const {VITE_API_KEY, VITE_AUTH_DOMAIN, VITE_PROJECT_ID, VITE_STORAGE_BUCKET, VITE_MESSAGEING_SENDER_ID, VITE_APP_ID, VITE_MEASUREMENT_ID} = process.env
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
+// const {VITE_API_KEY, VITE_AUTH_DOMAIN, VITE_PROJECT_ID, VITE_STORAGE_BUCKET, VITE_MESSAGEING_SENDER_ID, VITE_APP_ID, VITE_MEASUREMENT_ID} = process.env
 
 const firebaseConfig = {
-  apiKey: VITE_API_KEY,
-  authDomain: VITE_AUTH_DOMAIN,
-  projectId: VITE_PROJECT_ID,
-  storageBucket: VITE_STORAGE_BUCKET,
-  messagingSenderId: VITE_MESSAGEING_SENDER_ID,
-  appId: VITE_APP_ID, 
-  measurementId: VITE_MEASUREMENT_ID
+  apiKey:  import.meta.env.VITE_API_KEY,
+  authDomain:  import.meta.env.VITE_AUTH_DOMAIN,
+  projectId:  import.meta.env.VITE_PROJECT_ID,
+  storageBucket:  import.meta.env.VITE_STORAGE_BUCKET,
+  messagingSenderId:  import.meta.env.VITE_MESSAGEING_SENDER_ID,
+  appId:  import.meta.env.VITE_APP_ID, 
+  measurementId:  import.meta.env.VITE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
