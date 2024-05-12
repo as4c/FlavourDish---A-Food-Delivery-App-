69abc0a77c0cbc9db94630e2fad08c82d523dd51
as4c <sagarkumarsameer@gmail.com>
2024-05-12 16:27:24 +0530

    payment fixed

--- client/src/pages/menuPage/Payment.jsx
+++ client/src/pages/menuPage/Payment.jsx
@@ -1,20 +1,32 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Elements } from "@stripe/react-stripe-js";
 import { loadStripe } from "@stripe/stripe-js";
 import CheckoutForm from "./CheckoutForm";
 import useCart from "../../hooks/useCart";
 
-const stripePromise = loadStripe(import.meta.env.VITE_STRIPE_PUB_KEY);
-
 const Payment = () => {
   const [cart] = useCart();
 
-  console.log(cart);
   //   calculate final price
   const cartTotal = cart.reduce((sum, item) => sum + item.price, 0);
   const totalPrice = parseFloat(cartTotal.toFixed(2));
 
-  //   console.log(totalPrice);
+  const [stripePromise, setStripePromise] = useState(null);
+
+  useEffect(() => {
+    const loadStripePromise = async () => {
+      const stripe = await loadStripe(import.meta.env.VITE_STRIPE_PUB_KEY);
+      setStripePromise(stripe);
+    };
+
+    loadStripePromise();
+  }, []);
+
+  // Render the component only when stripePromise is resolved
+  if (!stripePromise) {
+    return <div>Loading...</div>;
+  }
+
   return (
     <div className="max-w-screen-2xl container mx-auto xl:px-24 px-4 py-28">
       <Elements stripe={stripePromise}>
